f3060d9072867dc8012efe6a7434c5b45c917077
Karteek Kuridi <KK00444226@techmahindra.com>
2017-09-22 11:43:50 +0530

    committed new code

---
+++ MonitorPipeline/src/com/techm/main/Main.java
@@ -0,0 +1,14 @@
+package com.techm.main;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.WebResource;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Client client = Client.create();
+		WebResource webResource = client.resource("http://10.14.129.6:8080/api/json");
+	}
+
+}
